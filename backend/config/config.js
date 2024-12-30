6d4b492ea42b1009c77f75833bd03e05f62c4711
liavjulio <liavjulio7@gmail.com>
2024-12-31 01:00:28 +0200

    fix

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 module.exports = {
-  development: {
-    username: process.env.DB_DEV_USER || "postgres",
-    password: process.env.DB_DEV_PASS || "postgres",
-    database: process.env.DB_DEV_NAME || "flight_booking_dev",
-    host: process.env.DB_DEV_HOST || "127.0.0.1",
-    dialect: "postgres"
-  },
-  test: {
-    username: process.env.DB_TEST_USER ,
-    password: process.env.DB_TEST_PASS ,
-    database: process.env.DB_TEST_NAME ,
-    host: process.env.DB_TEST_HOST ,
-    dialect: "postgres"
-  },
-  production: {
-    username: process.env.DB_PROD_USER ,
-    password: process.env.DB_PROD_PASS ,
-    database: process.env.DB_PROD_NAME ,
-    host: process.env.DB_PROD_HOST ,
-    dialect: "postgres"
-  }
+    development: {
+        username: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASS || 'postgres',
+        database: process.env.DB_NAME || 'flight_booking_dev',
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+    },
+    test: {
+        username: process.env.DB_TEST_USER || 'postgres',
+        password: process.env.DB_TEST_PASS || 'postgres',
+        database: process.env.DB_TEST_NAME || 'flight_booking_test',
+        host: process.env.DB_TEST_HOST || '127.0.0.1',
+        port: process.env.DB_TEST_PORT || 5432,
+        dialect: 'postgres',
+    },
+    production: {
+        username: process.env.DB_PROD_USER || 'postgres',
+        password: process.env.DB_PROD_PASS || 'postgres',
+        database: process.env.DB_PROD_NAME || 'flight_booking_prod',
+        host: process.env.DB_PROD_HOST || '127.0.0.1',
+        port: process.env.DB_PROD_PORT || 5432,
+        dialect: 'postgres',
+    },
 };
