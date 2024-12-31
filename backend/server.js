204a9dcc852a6411909ba3b2021ae5e2bd121b82
liavjulio <liavjulio7@gmail.com>
2024-12-31 03:59:48 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+// Allow requests from the frontend origin
+const corsOptions = {
+    origin: 'http://localhost:3001', // Frontend URL
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+};
 
+app.use(cors(corsOptions));
 // Routes
 const flightRoutes = require('./routes/flightRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
