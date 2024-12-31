85cb6e7e7c445003e6c102511989dae74a406fe0
liavjulio <liavjulio7@gmail.com>
2024-12-31 02:01:11 +0200

    debug

--- backend/tests/flightRoutes.test.js
+++ backend/tests/flightRoutes.test.js
@@ -13,6 +13,7 @@ afterAll(async () => {
 
 describe('Flight Routes', () => {
     console.log('Current NODE_ENV:', process.env.NODE_ENV);
+    console.log('Current NODE_DB:', process.env.DB_TEST_NAME);
     it('should fetch all flights', async () => {
         const res = await request(app).get('/flights');
         expect(res.statusCode).toEqual(200);
