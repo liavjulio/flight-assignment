376f18831b09f54bdd8d957d4e7385572389be6b
liavjulio <liavjulio7@gmail.com>
2024-12-31 13:05:56 +0200

    add root api

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ const bookingRoutes = require('./routes/bookingRoutes');
 
 app.use('/flights', flightRoutes);
 app.use('/bookings', bookingRoutes);
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Flight Booking API');
+});
 // Start the server only if not in test environment
 if (process.env.NODE_ENV !== 'test') {
     app.listen(port, () => {
